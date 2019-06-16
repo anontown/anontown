73434d0399035f1414e67444787716003c38734a
kgtkr <kgtkr.jp@gmail.com>
2019-06-16 12:48:10 +0900

    マイグレーションで何も出力されない問題と、終了しない問題修正

--- packages/server/src/migrate.app.ts
+++ packages/server/src/migrate.app.ts
@@ -7,8 +7,11 @@ import { Config } from "./config";
   const migrateFile = path.join(Config.saveDir, "./data/.migrate.json");
 
   const ver = await fs.readFile(migrateFile, "utf8").then(json => JSON.parse(json) as number).catch(() => 0);
+  console.log("current db-version", ver);
   const newVer = await migrate(ver);
+  console.log("updated db-version", newVer);
   await fs.writeFile(migrateFile, JSON.stringify(newVer), {
     encoding: "utf8",
   });
+  process.exit(0);
 })();
