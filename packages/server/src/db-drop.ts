518a93d2f304082fbd4b4fac7342c373158c7f74
kgtkr <kgtkr.jp@gmail.com>
2019-06-16 12:54:06 +0900

    dbDrop修正

--- packages/server/src/db-drop.ts
+++ packages/server/src/db-drop.ts
@@ -2,19 +2,12 @@ import { DB, ESClient } from "./db";
 
 export async function dbDrop() {
   const db = await DB();
+  const cls = await db.collections();
+  for (const cl of cls) {
+    if (cl.collectionName.indexOf("system.") !== 0) {
+      await cl.drop();
+    }
+  }
 
-  await db.collection("clients").drop();
-  await db.collection("profiles").drop();
-  await db.collection("tokens").drop();
-  await db.collection("users").drop();
-  await db.collection("storages").drop();
-
-  const es = ESClient();
-
-  await es.indices.deleteAlias({ name: "reses", index: "reses_1" });
-  await es.indices.deleteAlias({ name: "histories", index: "histories_1" });
-  await es.indices.deleteAlias({ name: "msgs", index: "msgs_1" });
-  await es.indices.deleteAlias({ name: "topics", index: "topics_1" });
-  await es.indices.delete({ index: ["reses_1", "histories_1", "msgs_1", "topics_1"] });
-  await es.deleteTemplate({ id: "template" });
+  await ESClient().indices.delete({ index: "*" });
 }
