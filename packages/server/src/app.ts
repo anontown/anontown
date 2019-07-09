0dae8900cb1a550c0e12a795ab60d303b1486948
kgtkr <kgtkr.jp@gmail.com>
2019-07-09 21:37:04 +0900

    rename

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-var-requires */
 require("source-map-support").install();
-import { Repo } from "../repositories";
-import { serverRun } from "../server";
+import { Repo } from "./repositories";
+import { serverRun } from "./server";
 
 /* tslint:disable:no-floating-promises */
 (async () => {
