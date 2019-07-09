af3e3e51c1e3419630af08ac4e304d1cde8469e1
kgtkr <kgtkr.jp@gmail.com>
2019-07-09 22:31:30 +0900

    ProfilesPageをhook化

--- packages/client/src/pages/profiles.tsx
+++ packages/client/src/pages/profiles.tsx
@@ -4,36 +4,36 @@ import { Helmet } from "react-helmet";
 import { RouteComponentProps, withRouter } from "react-router";
 import { Page, ProfileAdd, ProfileEditor, Snack } from "../components";
 import * as G from "../generated/graphql";
-import { userSwitch, UserSwitchProps } from "../utils";
+import { userSwitch, UserSwitchProps, queryResultConvert } from "../utils";
 
 type ProfilesPageProps = RouteComponentProps & UserSwitchProps;
 
 export const ProfilesPage = userSwitch(
   withRouter((props: ProfilesPageProps) => {
+    const profiles = G.useFindProfilesQuery({
+      variables: {
+        query: {
+          self: true,
+        },
+      },
+    });
+    queryResultConvert(profiles);
+
     return (
       <Page>
         <Helmet title="プロフィール管理" />
         <Tabs>
           <Tab label="編集">
-            <G.FindProfilesComponent variables={{ query: { self: true } }}>
-              {({ loading, error, data }) => {
-                if (loading) {
-                  return "Loading...";
-                }
-                if (error || !data) {
-                  return <Snack msg="プロフィール取得に失敗しました" />;
-                }
-
-                return data.profiles.map(p => (
+            {profiles.data !== undefined
+              ? profiles.data.profiles.map(p => (
                   <ProfileEditor
                     style={{ marginBottom: 10 }}
                     key={p.id}
                     profile={p}
                     userData={props.userData}
                   />
-                ));
-              }}
-            </G.FindProfilesComponent>
+                ))
+              : null}
           </Tab>
           <Tab label="新規">
             <ProfileAdd userData={props.userData} />
