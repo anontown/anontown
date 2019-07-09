eed6fd80f146757cb7b27782c76c6aa386f64bcd
kgtkr <kgtkr.jp@gmail.com>
2019-07-09 22:24:52 +0900

    ProfilesPageを関数コンポーネントにした

--- packages/client/src/pages/profiles.tsx
+++ packages/client/src/pages/profiles.tsx
@@ -8,48 +8,38 @@ import { userSwitch, UserSwitchProps } from "../utils";
 
 type ProfilesPageProps = RouteComponentProps & UserSwitchProps;
 
-export interface ProfilesPageState {}
-
 export const ProfilesPage = userSwitch(
-  withRouter(
-    class extends React.Component<ProfilesPageProps, ProfilesPageState> {
-      constructor(props: ProfilesPageProps) {
-        super(props);
-      }
+  withRouter((props: ProfilesPageProps) => {
+    return (
+      <Page>
+        <Helmet title="プロフィール管理" />
+        <Tabs>
+          <Tab label="編集">
+            <G.FindProfilesComponent variables={{ query: { self: true } }}>
+              {({ loading, error, data }) => {
+                if (loading) {
+                  return "Loading...";
+                }
+                if (error || !data) {
+                  return <Snack msg="プロフィール取得に失敗しました" />;
+                }
 
-      render() {
-        return (
-          <Page>
-            <Helmet title="プロフィール管理" />
-            <Tabs>
-              <Tab label="編集">
-                <G.FindProfilesComponent variables={{ query: { self: true } }}>
-                  {({ loading, error, data }) => {
-                    if (loading) {
-                      return "Loading...";
-                    }
-                    if (error || !data) {
-                      return <Snack msg="プロフィール取得に失敗しました" />;
-                    }
-
-                    return data.profiles.map(p => (
-                      <ProfileEditor
-                        style={{ marginBottom: 10 }}
-                        key={p.id}
-                        profile={p}
-                        userData={this.props.userData}
-                      />
-                    ));
-                  }}
-                </G.FindProfilesComponent>
-              </Tab>
-              <Tab label="新規">
-                <ProfileAdd userData={this.props.userData} />
-              </Tab>
-            </Tabs>
-          </Page>
-        );
-      }
-    },
-  ),
+                return data.profiles.map(p => (
+                  <ProfileEditor
+                    style={{ marginBottom: 10 }}
+                    key={p.id}
+                    profile={p}
+                    userData={props.userData}
+                  />
+                ));
+              }}
+            </G.FindProfilesComponent>
+          </Tab>
+          <Tab label="新規">
+            <ProfileAdd userData={props.userData} />
+          </Tab>
+        </Tabs>
+      </Page>
+    );
+  }),
 );
