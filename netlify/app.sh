4c86b6ae2c20c1812cb1e2f180d567739ca3cdc2
kgtkr <kgtkr.jp@gmail.com>
2019-08-05 17:16:15 +0900

    デプロイ設定修正

--- netlify/app.sh
+++ netlify/app.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 npm i
 npx lerna bootstrap
+lerna exec --scope @anontown/route -- npm run build
 ENV_NAME=prod lerna exec --scope @anontown/client -- npm run build
 mv packages/client/dist deploy
