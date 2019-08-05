d101bd90eaafd9cc345674a44abc65fd89643067
kgtkr <kgtkr.jp@gmail.com>
2019-08-05 21:32:37 +0900

    tagsLinkのバグ修正

--- packages/client/src/components/tags-link.tsx
+++ packages/client/src/components/tags-link.tsx
@@ -13,11 +13,14 @@ export function TagsLink(props: TagsLinkProps) {
   return (
     <Link
       className={props.mini ? style.mini : undefined}
-      to={routes.topicSearch.to({
-        query: {
-          tags: props.tags,
+      to={routes.topicSearch.to(
+        {},
+        {
+          query: {
+            tags: props.tags,
+          },
         },
-      })}
+      )}
     >
       {props.tags.length !== 0 ? props.tags.join(",") : "(なし)"}
     </Link>
