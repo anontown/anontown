fe285f1325e9fbc331fab58ca4bb10ba41fdc359
kgtkr <kgtkr.jp@gmail.com>
2019-08-07 00:09:26 +0900

    サイズ変更

--- packages/icon/build.js
+++ packages/icon/build.js
@@ -2,8 +2,15 @@ const fs = require("pn/fs");
 const svg2png = require("svg2png");
 
 (async () => {
+  try {
+    await fs.mkdir("dist");
+  } catch {}
+
   const data = await fs.readFile("icon.svg");
-  for (const size of [4, 5, 6, 7, 8, 9, 10, 11, 12].map(x => 2 ** x)) {
-    await fs.writeFile(`${size}.png`, await svg2png(data, { width: size, height: size }));
+  for (const size of [4, 5, 6, 7, 8, 9, 10].map(x => 2 ** x)) {
+    await fs.writeFile(
+      `dist/${size}.png`,
+      await svg2png(data, { width: size, height: size })
+    );
   }
 })();
