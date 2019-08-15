30840dfcf03ff136848d000cff013a1a6bcf8e16
kgtkr <kgtkr.jp@gmail.com>
2019-08-16 01:38:21 +0900

    Profileのfc化

--- packages/client/src/components/profile.tsx
+++ packages/client/src/components/profile.tsx
@@ -6,20 +6,12 @@ export interface ProfileProps {
   profile: G.ProfileFragment;
 }
 
-interface ProfileState {}
-
-export class Profile extends React.Component<ProfileProps, ProfileState> {
-  constructor(props: ProfileProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.profile.name}●{this.props.profile.sn}
-        <hr />
-        <Md text={this.props.profile.text} />
-      </div>
-    );
-  }
+export function Profile(props: ProfileProps) {
+  return (
+    <div>
+      {props.profile.name}●{props.profile.sn}
+      <hr />
+      <Md text={props.profile.text} />
+    </div>
+  );
 }
