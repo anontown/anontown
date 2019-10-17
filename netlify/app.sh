f49e5494b1242a1c58c0bced14c79ef01cdf5ed0
kgtkr <kgtkr.jp@gmail.com>
2019-10-17 12:33:54 +0900

    netlify関連のファイル削除

--- netlify/app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd client
-npm ci --no-progress
-npx lerna bootstrap --ci --no-progress
-ENV_NAME=prod lerna run build --scope @anontown/client --include-filtered-dependencies
-mv packages/client/dist ../deploy
