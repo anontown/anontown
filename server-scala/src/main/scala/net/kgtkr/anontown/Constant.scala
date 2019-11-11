b19971a0cd3830a18ea983833872a9c757e4075a
kgtkr <kgtkr.jp@gmail.com>
2019-11-11 19:39:13 +0900

    エラー修正

--- server-scala/src/main/scala/net/kgtkr/anontown/Constant.scala
+++ server-scala/src/main/scala/net/kgtkr/anontown/Constant.scala
@@ -4,25 +4,25 @@ import java.util.regex.Pattern;
 
 object Constant {
   object User {
-    val snRegex: RegexValidate = RegexValidate(
+    val snRegex: RegexValidator = RegexValidator(
       Pattern.compile("[a-zA-Z0-9_]{3,20}"),
       "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい"
     );
 
-    val sn: StringValidate =
-      StringValidate(
+    val sn: StructureValidator =
+      StructureValidator(
         List(CharType.Lc(), CharType.Uc(), CharType.D(), CharType.Ub()),
         Some(3),
         Some(20)
       );
 
-    val passRegex: RegexValidate = RegexValidate(
+    val passRegex: RegexValidator = RegexValidator(
       Pattern.compile("[a-zA-Z0-9_]{3,50}"),
       "パスワードは半角英数字、アンダーバー3～50文字にして下さい"
     );
 
-    val pass: StringValidate =
-      StringValidate(
+    val pass: StructureValidator =
+      StructureValidator(
         List(CharType.Lc(), CharType.Uc(), CharType.D(), CharType.Ub()),
         Some(3),
         Some(50)
@@ -32,37 +32,37 @@ object Constant {
   }
 
   object Profile {
-    val nameRegex: RegexValidate = RegexValidate(
+    val nameRegex: RegexValidator = RegexValidator(
       Pattern.compile("[\\S]{1,50}"),
       "名前は1～50文字にして下さい"
     );
 
-    val name: StringValidate =
-      StringValidate(
+    val name: StructureValidator =
+      StructureValidator(
         List(CharType.NotNewLine()),
         Some(1),
         Some(50)
       );
 
-    val snRegex: RegexValidate = RegexValidate(
+    val snRegex: RegexValidator = RegexValidator(
       Pattern.compile("[a-zA-Z0-9_]{3,20}"),
       "スクリーンネームは半角英数字、アンダーバー3～20文字にして下さい"
     );
 
-    val sn: StringValidate =
-      StringValidate(
+    val sn: StructureValidator =
+      StructureValidator(
         List(CharType.Lc(), CharType.Uc(), CharType.D(), CharType.Ub()),
         Some(3),
         Some(20)
       );
 
-    val textRegex: RegexValidate = RegexValidate(
+    val textRegex: RegexValidator = RegexValidator(
       Pattern.compile("[\\s\\S]{1,3000}"),
       "自己紹介文は1～3000文字にして下さい"
     );
 
-    val text: StringValidate =
-      StringValidate(
+    val text: StructureValidator =
+      StructureValidator(
         List(CharType.NotNewLine(), CharType.NewLine()),
         Some(1),
         Some(3000)
@@ -70,12 +70,12 @@ object Constant {
   }
 
   object Token {
-    val nameRegex: RegexValidate = RegexValidate(
+    val nameRegex: RegexValidator = RegexValidator(
       Pattern.compile("[\\S]{1,50}"),
       "名前は1～50文字にして下さい"
     );
 
-    val name: StringValidate = StringValidate(
+    val name: StructureValidator = StructureValidator(
       List(CharType.NotNewLine()),
       Some(1),
       Some(50)
@@ -85,25 +85,25 @@ object Constant {
   }
 
   object Storage {
-    val keyRegex: RegexValidate =
-      RegexValidate(
+    val keyRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\s\\S]{1,100}"),
         "ストレージキーは1～100文字にして下さい"
       );
 
-    val key: StringValidate = StringValidate(
+    val key: StructureValidator = StructureValidator(
       List(CharType.NewLine(), CharType.NotNewLine()),
       Some(1),
       Some(100)
     );
 
-    val valueRegex: RegexValidate =
-      RegexValidate(
+    val valueRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\s\\S]{0,100000}"),
         "ストレージの値は0～100000文字にして下さい"
       );
 
-    val value: StringValidate = StringValidate(
+    val value: StructureValidator = StructureValidator(
       List(CharType.NewLine(), CharType.NotNewLine()),
       Some(0),
       Some(100000)
@@ -111,45 +111,45 @@ object Constant {
   }
 
   object Client {
-    val nameRegex: RegexValidate =
-      RegexValidate(
+    val nameRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\S]{1,30}"),
         "名前は1～30文字にして下さい"
       );
 
-    val name: StringValidate = StringValidate(
+    val name: StructureValidator = StructureValidator(
       List(CharType.NotNewLine()),
       Some(1),
       Some(30)
     );
 
-    val urlRegex: RegexValidate =
-      RegexValidate(
+    val urlRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("https?:\\/\\/.{1,500}"),
         "URLが不正です"
       );
   }
 
   object Topic {
-    val titleRegex: RegexValidate =
-      RegexValidate(
+    val titleRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\S]{1,100}"),
         "タイトルは1～100文字にして下さい"
       );
 
-    val title: StringValidate = StringValidate(
+    val title: StructureValidator = StructureValidator(
       List(CharType.NotNewLine()),
       Some(1),
       Some(100)
     );
 
-    val tagRegex: RegexValidate =
-      RegexValidate(
+    val tagRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[a-z0-9ぁ-んァ-ヶー一-龠々_]{1,20}"),
         "タグは半角小文字英数字ひらがな漢字、アンダーバー1～20文字15個以内にして下さい"
       );
 
-    val tag: StringValidate = StringValidate(
+    val tag: StructureValidator = StructureValidator(
       List(
         CharType.Lc(),
         CharType.D(),
@@ -164,13 +164,13 @@ object Constant {
 
     val tagMax: Int = 15;
 
-    val textRegex: RegexValidate =
-      RegexValidate(
+    val textRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\S\\s]{1,10000}"),
         "本文は1～1万文字以内にして下さい"
       );
 
-    val text: StringValidate = StringValidate(
+    val text: StructureValidator = StructureValidator(
       List(CharType.NotNewLine(), CharType.NewLine()),
       Some(1),
       Some(10000)
@@ -180,25 +180,25 @@ object Constant {
   }
 
   object Res {
-    val nameRegex: RegexValidate =
-      RegexValidate(
+    val nameRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\S]{1,50}"),
         "名前は50文字以内にして下さい"
       );
 
-    val name: StringValidate = StringValidate(
+    val name: StructureValidator = StructureValidator(
       List(CharType.NotNewLine()),
       Some(1),
       Some(50)
     );
 
-    val textRegex: RegexValidate =
-      RegexValidate(
+    val textRegex: RegexValidator =
+      RegexValidator(
         Pattern.compile("[\\S\\s]{1,5000}"),
         "本文は1～5000文字にして下さい"
       );
 
-    val text: StringValidate = StringValidate(
+    val text: StructureValidator = StructureValidator(
       List(CharType.NotNewLine(), CharType.NewLine()),
       Some(1),
       Some(5000)
