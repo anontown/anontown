e171a2dd35676a269fcaa0229dd0d46f38addaae
kgtkr <kgtkr.jp@gmail.com>
2019-11-12 21:44:25 +0900

    TestHelper作った

--- server-scala/src/test/scala/net/kgtkr/anontown/entities/User.scala
+++ server-scala/src/test/scala/net/kgtkr/anontown/entities/User.scala
@@ -16,8 +16,7 @@ import net.kgtkr.anontown.ConfigFixtures
 import net.kgtkr.anontown.adapters.DummyObjectIdGenerator
 import net.kgtkr.anontown.adapters.DummyConfigContainerImpl
 import net.kgtkr.anontown.ports.ConfigContainer
-import zio.internal.PlatformLive
-import zio.Runtime
+import net.kgtkr.anontown.TestHelper
 
 object UserFixtures {
   val userID = new ObjectId().toHexString()
@@ -53,37 +52,41 @@ object UserFixtures {
 }
 
 class UserSpec extends FlatSpec with Matchers {
-  "create" should "正常に作れるか" in {
-    Runtime(
-      new ObjectIdGeneratorComponent with ClockComponent
-      with ConfigContainerComponent {
-        val objectIdGenerator =
-          new DummyObjectIdGenerator(UserFixtures.userID)
+  def createPorts() =
+    new ObjectIdGeneratorComponent with ClockComponent
+    with ConfigContainerComponent {
+      val objectIdGenerator =
+        new DummyObjectIdGenerator(UserFixtures.userID)
 
-        val clock = new ClockImpl(
-          OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
-        )
-
-        val configContainer = new DummyConfigContainerImpl()
-      },
-      PlatformLive.Default
-    ).unsafeRun(
-      User
-        .create(
-          sn = "scn",
-          pass = "pass"
-        )
-    ) should be(
-      UserFixtures.user.copy(
-        resWait = UserFixtures.user.resWait.copy(
-          last =
-            OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
-        ),
-        lastTopic =
-          OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC),
-        lastOneTopic =
-          OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
+      val clock = new ClockImpl(
+        OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
       )
-    )
+
+      val configContainer = new DummyConfigContainerImpl()
+    }
+
+  "create" should "正常に作れるか" in {
+    TestHelper.runZioTest(createPorts()) {
+      (for {
+        user <- User
+          .create(
+            sn = "scn",
+            pass = "pass"
+          )
+      } yield {
+        user should be(
+          UserFixtures.user.copy(
+            resWait = UserFixtures.user.resWait.copy(
+              last = OffsetDateTime
+                .ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
+            ),
+            lastTopic =
+              OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC),
+            lastOneTopic =
+              OffsetDateTime.ofInstant(Instant.ofEpochMilli(0), ZoneOffset.UTC)
+          )
+        );
+      })
+    }
   }
 }
