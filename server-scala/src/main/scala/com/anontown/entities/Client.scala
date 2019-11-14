44c311105e23dc7aaf45d9a790e679d6440880ea
kgtkr <kgtkr.jp@gmail.com>
2019-11-14 15:57:29 +0900

    Cientのデータ型だけ

---
+++ server-scala/src/main/scala/com/anontown/entities/Client.scala
@@ -0,0 +1,50 @@
+package com.anontown.entities
+
+import java.time.OffsetDateTime
+import cats._, cats.implicits._, cats.derived._
+import com.anontown.utils.Impl._;
+import com.anontown.utils;
+import com.anontown.utils.OffsetDateTimeUtils._;
+import com.anontown.ports.SafeIdGeneratorComponent
+import zio.ZIO
+import com.anontown.AtServerError
+import com.anontown.ports.ConfigContainerComponent
+import com.anontown.AuthTokenMaster
+import com.anontown.AtError
+import com.anontown.AtTokenAuthError
+import com.anontown.AuthUser
+import com.anontown.ports.ObjectIdGeneratorComponent
+import com.anontown.ports.ClockComponent
+import com.anontown.Constant
+import com.anontown.AuthTokenGeneral
+import com.anontown.AtNotFoundError
+
+final case class ClientAPI(
+    id: String,
+    name: String,
+    url: String,
+    self: Option[Boolean],
+    date: String,
+    update: String
+);
+
+object ClientAPI {
+  implicit val eqImpl: Eq[ClientAPI] = {
+    import auto.eq._
+    semi.eq
+  }
+
+  implicit val showImpl: Show[ClientAPI] = {
+    import auto.show._
+    semi.show
+  }
+}
+
+final case class Client(
+    id: String,
+    name: String,
+    url: String,
+    user: String,
+    date: OffsetDateTime,
+    update: OffsetDateTime
+) {}
