079f35bf0ca282a701c3621b3cde4852f840f989
kgtkr <kgtkr.jp@gmail.com>
2019-11-19 21:58:24 +0900

    Topicのデータ型だけ作った

---
+++ server-scala/src/main/scala/com/anontown/entities/Topic.scala
@@ -0,0 +1,180 @@
+package com.anontown.entities
+
+import java.time.OffsetDateTime
+import cats._, cats.implicits._, cats.derived._
+import com.anontown.utils.Impl._;
+import zio.ZIO
+import com.anontown.AuthTokenMaster
+import com.anontown.AtError
+import com.anontown.ports.ObjectIdGeneratorComponent
+import com.anontown.ports.ClockComponent
+import com.anontown.Constant
+import com.anontown.AtParamsError
+import com.anontown.AtRightError
+import com.anontown.AuthToken
+import com.anontown.AtServerError
+
+final case class TagsAPI(name: String, count: Int);
+object TagsAPI {
+  implicit val eqImpl: Eq[TagsAPI] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+sealed trait TopicAPI {
+  val id: String;
+  val title: String;
+  val update: String;
+  val date: String;
+  val resCount: Int;
+  val active: Boolean;
+}
+
+sealed trait TopicSearchAPI extends TopicAPI {
+  val tags: List[String];
+  val text: String;
+}
+
+final case class TopicNormalAPI(
+    id: String,
+    title: String,
+    update: String,
+    date: String,
+    resCount: Int,
+    active: Boolean,
+    tags: List[String],
+    text: String
+) extends TopicSearchAPI;
+
+object TopicNormalAPI {
+  implicit val eqImpl: Eq[TopicNormalAPI] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+final case class TopicOneAPI(
+    id: String,
+    title: String,
+    update: String,
+    date: String,
+    resCount: Int,
+    active: Boolean,
+    tags: List[String],
+    text: String
+) extends TopicSearchAPI;
+
+object TopicOneAPI {
+  implicit val eqImpl: Eq[TopicOneAPI] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+final case class TopicForkAPI(
+    id: String,
+    title: String,
+    update: String,
+    date: String,
+    resCount: Int,
+    active: Boolean,
+    parentID: String
+) extends TopicAPI;
+
+object TopicForkAPI {
+  implicit val eqImpl: Eq[TopicForkAPI] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+sealed trait TopicId extends Any {
+  def value: String;
+}
+
+sealed trait TopicSearchId extends Any with TopicId;
+
+final case class TopicForkId(value: String) extends AnyVal with TopicId;
+object TopicForkId {
+  implicit val eqImpl: Eq[TopicForkId] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+final case class TopicNormalId(value: String) extends AnyVal with TopicSearchId;
+object TopicNormalId {
+  implicit val eqImpl: Eq[TopicNormalId] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+final case class TopicOneId(value: String) extends AnyVal with TopicSearchId;
+object TopicOneId {
+  implicit val eqImpl: Eq[TopicOneId] = {
+    import auto.eq._
+    semi.eq
+  }
+}
+
+sealed trait Topic {
+  type Id <: TopicId;
+
+  val id: Id;
+  val title: String;
+  val update: OffsetDateTime;
+  val date: OffsetDateTime;
+  val resCount: Int;
+  val ageUpdate: OffsetDateTime;
+  val active: Boolean;
+}
+
+sealed trait TopicSearch extends Topic {
+  type Id <: TopicSearchId;
+
+  val tags: List[String];
+  val text: String;
+}
+
+final case class TopicNormal(
+    id: TopicNormalId,
+    title: String,
+    update: OffsetDateTime,
+    date: OffsetDateTime,
+    resCount: Int,
+    ageUpdate: OffsetDateTime,
+    active: Boolean,
+    tags: List[String],
+    text: String
+) extends TopicSearch {
+  type Id = TopicNormalId;
+}
+
+final case class TopicOne(
+    id: TopicOneId,
+    title: String,
+    update: OffsetDateTime,
+    date: OffsetDateTime,
+    resCount: Int,
+    ageUpdate: OffsetDateTime,
+    active: Boolean,
+    tags: List[String],
+    text: String
+) extends TopicSearch {
+  type Id = TopicOneId;
+}
+
+final case class TopicFork(
+    id: TopicForkId,
+    title: String,
+    update: OffsetDateTime,
+    date: OffsetDateTime,
+    resCount: Int,
+    ageUpdate: OffsetDateTime,
+    active: Boolean,
+    parent: TopicNormalId
+) extends Topic {
+  type Id = TopicForkId;
+}
