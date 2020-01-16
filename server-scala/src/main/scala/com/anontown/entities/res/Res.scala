b6a2f4b2b58e410f9b19e7c2e02699c7a1bf7fbe
kgtkr <kgtkr.jp@gmail.com>
2020-01-16 22:55:45 +0900

    remove unused import

--- server-scala/src/main/scala/com/anontown/entities/res/Res.scala
+++ server-scala/src/main/scala/com/anontown/entities/res/Res.scala
@@ -1,35 +1,19 @@
 package com.anontown.entities.res
 
 import java.time.OffsetDateTime
-import cats._, cats.implicits._, cats.derived._
-import com.anontown.utils.Impl._;
-import zio.ZIO
+import cats.implicits._
 import com.anontown.AtError
-import com.anontown.ports.ObjectIdGeneratorComponent
-import com.anontown.ports.ClockComponent
 import com.anontown.AtRightError
 import com.anontown.AuthToken
 import com.anontown.AtPrerequisiteError
 import simulacrum._
-import monocle.macros.syntax.lens._
 import monocle.syntax.ApplyLens
 import Res.ops._;
 import shapeless._
 import record._
 import com.anontown.utils.Record._
 import com.anontown.entities.user.{UserId, User}
-import com.anontown.entities.topic.{
-  TopicId,
-  Topic,
-  TopicNormalId,
-  TopicTemporaryId,
-  TopicNormal,
-  TopicTemporary,
-  TopicForkId,
-  TopicFork
-}
-import com.anontown.entities.profile.{ProfileId, Profile}
-import com.anontown.entities.history.{HistoryId, History}
+import com.anontown.entities.topic.TopicId
 
 trait ResAPI {
   val id: String;
