d2828a3fe3da9505f1e27ad0c2c3455e7b475bd2
kgtkr <kgtkr.jp@gmail.com>
2020-01-18 22:15:48 +0900

    remove unused import

--- server-scala/src/main/scala/com/anontown/entities/res/ResTopicId.scala
+++ server-scala/src/main/scala/com/anontown/entities/res/ResTopicId.scala
@@ -1,35 +1,6 @@
 package com.anontown.entities.res
 
-import java.time.OffsetDateTime
 import cats._, cats.implicits._, cats.derived._
-import com.anontown.utils.Impl._;
-import zio.ZIO
-import com.anontown.AtError
-import com.anontown.ports.ObjectIdGeneratorComponent
-import com.anontown.ports.ClockComponent
-import com.anontown.AtRightError
-import com.anontown.AuthToken
-import com.anontown.AtPrerequisiteError
-import simulacrum._
-import monocle.macros.syntax.lens._
-import monocle.syntax.ApplyLens
-import Res.ops._;
-import shapeless._
-import record._
-import com.anontown.utils.Record._
-import com.anontown.entities.user.{UserId, User}
-import com.anontown.entities.topic.{
-  TopicId,
-  Topic,
-  TopicNormalId,
-  TopicTemporaryId,
-  TopicNormal,
-  TopicTemporary,
-  TopicForkId,
-  TopicFork
-}
-import com.anontown.entities.profile.{ProfileId, Profile}
-import com.anontown.entities.history.{HistoryId, History}
 
 final case class ResTopicId(value: String) extends AnyVal with ResId;
 
