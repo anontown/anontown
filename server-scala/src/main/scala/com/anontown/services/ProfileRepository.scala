b080207551ee341b279988d1874cd6bf743a72a1
kgtkr <kgtkr.jp@gmail.com>
2020-02-15 20:25:39 +0900

    ProfileRepository

---
+++ server-scala/src/main/scala/com/anontown/services/ProfileRepository.scala
@@ -0,0 +1,18 @@
+package com.anontown.services
+
+import cats.tagless.finalAlg
+import com.anontown.entities.profile.ProfileId
+import cats.data.EitherT
+import com.anontown.AtError
+import com.anontown.entities.profile.Profile
+
+@finalAlg
+trait ProfileRepository[F[_]] {
+  def findOne(id: ProfileId): EitherT[F, AtError, Profile];
+  def find(
+      id: Option[List[ProfileId]],
+      self: Option[Boolean]
+  ): EitherT[F, AtError, List[Profile]];
+  def insert(profile: Profile): EitherT[F, AtError, Unit];
+  def update(profile: Profile): EitherT[F, AtError, Unit];
+}
