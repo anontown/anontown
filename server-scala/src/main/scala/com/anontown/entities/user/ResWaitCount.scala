2ee78d3908383126d9990a2fddf3a56028eeb868
kgtkr <kgtkr.jp@gmail.com>
2020-03-02 16:07:41 +0900

    UserRepositoryAlgを作った

--- server-scala/src/main/scala/com/anontown/entities/user/ResWaitCount.scala
+++ server-scala/src/main/scala/com/anontown/entities/user/ResWaitCount.scala
@@ -2,6 +2,22 @@ package com.anontown.entities.user
 
 import cats._, cats.implicits._, cats.derived._
 
+sealed trait ResWaitCountKey;
+
+object ResWaitCountKey {
+  implicit val implEq: Eq[ResWaitCountKey] = {
+    import auto.eq._
+    semi.eq
+  }
+
+  final case class M10() extends ResWaitCountKey;
+  final case class M30() extends ResWaitCountKey;
+  final case class H1() extends ResWaitCountKey;
+  final case class H6() extends ResWaitCountKey;
+  final case class H12() extends ResWaitCountKey;
+  final case class D1() extends ResWaitCountKey;
+}
+
 final case class ResWaitCount(
     m10: Int,
     m30: Int,
---
+++ server-scala/src/main/scala/com/anontown/services/UserRepository.scala
@@ -0,0 +1,19 @@
+package com.anontown.services
+
+import cats.tagless.finalAlg
+import com.anontown.entities.user.UserId
+import cats.data.EitherT
+import com.anontown.AtError
+import com.anontown.entities.user.UserSn
+import com.anontown.entities.user.User
+import com.anontown.entities.user.ResWaitCountKey
+
+@finalAlg
+trait UserRepositoryAlg[F[_]] {
+  def findOne(id: UserId): EitherT[F, AtError, User];
+  def findID(sn: UserSn): EitherT[F, AtError, UserId];
+  def insert(user: User): EitherT[F, AtError, Unit];
+  def update(user: User): EitherT[F, AtError, Unit];
+  def cronPointReset(): EitherT[F, AtError, Unit];
+  def cronCountReset(key: ResWaitCountKey): EitherT[F, AtError, Unit];
+}
