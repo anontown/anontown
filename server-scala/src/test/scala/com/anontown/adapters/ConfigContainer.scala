97fff9a5b4d318b9930cdc8e26572637cfc5bce7
kgtkr <kgtkr.jp@gmail.com>
2020-03-09 15:06:33 +0900

    test一旦削除

--- server-scala/src/test/scala/com/anontown/adapters/ConfigContainer.scala
+++
@@ -1,8 +0,0 @@
-package com.anontown.adapters
-import com.anontown.Config
-import com.anontown.ports.ConfigContainer
-import com.anontown.ConfigFixtures
-
-class DummyConfigContainerImpl extends ConfigContainer {
-  val config: Config = ConfigFixtures.config
-}
