990ccc6380251f7458e8dbf078cb452630873172
kgtkr <kgtkr.jp@gmail.com>
2020-03-16 23:59:50 +0900

    Subscriptionの設定

--- server-scala/src/main/scala/com/anontown/application/resolvers/AppSchema.scala
+++ server-scala/src/main/scala/com/anontown/application/resolvers/AppSchema.scala
@@ -13,7 +13,17 @@ object AppSchema {
       deriveContextObjectType[Ctx, Query, Unit](ctx => new Query(ctx))
     val mutationType =
       deriveContextObjectType[Ctx, Mutation, Unit](ctx => new Mutation(ctx))
-    Schema(query = queryType, mutation = Some(mutationType))
+    Schema(
+      query = queryType,
+      mutation = Some(mutationType),
+      subscription = Some({
+        // TODO: 外から設定できるべき
+        import scala.concurrent.ExecutionContext.Implicits.global
+        import cats.effect.IO.contextShift
+        implicit val cs = contextShift(implicitly)
+        Subscription.subscriptionType
+      })
+    )
 
   }
 }
