05e5c11ca5a3077762032ab53c499b3a54e4152c
kgtkr <kgtkr.jp@gmail.com>
2020-03-17 00:15:09 +0900

    ResEventSubscriberAlgがG[_]を取らないようにした

--- server-scala/src/main/scala/com/anontown/ports/ResEventSubscriber.scala
+++ server-scala/src/main/scala/com/anontown/ports/ResEventSubscriber.scala
@@ -4,8 +4,9 @@ import cats.tagless.finalAlg
 import cats.data.EitherT
 import com.anontown.AtServerError
 import com.anontown.entities.res.Res
+import fs2.Stream
 
 @finalAlg
-trait ResEventSubscriberAlg[F[_], G[_]] {
-  def subscribeAddRes(): EitherT[F, AtServerError, G[Res]];
+trait ResEventSubscriberAlg[F[_]] {
+  def subscribeAddRes(): Stream[F, Res];
 }
