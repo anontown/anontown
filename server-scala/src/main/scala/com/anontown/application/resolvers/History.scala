cc4e14c2d6a65895d0296d8e51049bdac2e5f99a
kgtkr <kgtkr.jp@gmail.com>
2020-03-17 22:55:58 +0900

    Historyのスキーマ

---
+++ server-scala/src/main/scala/com/anontown/application/resolvers/History.scala
@@ -0,0 +1,25 @@
+package com.anontown.application.resolvers
+
+import sangria.macros.derive.GraphQLOutputType
+import sangria.schema.IDType
+import sangria.macros.derive.GraphQLExclude
+import com.anontown.entities.DateTime
+import sangria.macros.derive._
+import com.anontown.application.resolvers.ScalarTypes.dateTimeType
+
+final case class History(
+    @GraphQLOutputType(IDType) id: String,
+    @GraphQLExclude topicId: String,
+    title: String,
+    tags: List[String],
+    text: String,
+    date: DateTime,
+    hash: String,
+    self: Option[Boolean]
+) {
+  // TODO: topic: TopicNormal
+}
+
+object History {
+  implicit val historyType = deriveObjectType[Ctx, History]()
+}
