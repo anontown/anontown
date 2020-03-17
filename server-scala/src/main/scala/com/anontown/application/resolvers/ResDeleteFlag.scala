622ceeebe44b50c6d8962829d58c13fa1198425f
kgtkr <kgtkr.jp@gmail.com>
2020-03-17 23:10:56 +0900

    ResDeleteFlagのスキーマ定義

---
+++ server-scala/src/main/scala/com/anontown/application/resolvers/ResDeleteFlag.scala
@@ -0,0 +1,21 @@
+package com.anontown.application.resolvers
+
+import sangria.macros.derive.GraphQLName
+import sangria.macros.derive.GraphQLDescription
+import sangria.macros.derive.GraphQLOutputType
+import sangria.schema.IDType
+import sangria.macros._
+import sangria.macros.derive._
+import com.anontown.entities.DateTime
+import com.anontown.application.resolvers.ScalarTypes.dateTimeType
+
+sealed trait ResDeleteFlag;
+
+object ResDeleteFlag {
+  @GraphQLName("self")
+  final case object Self extends ResDeleteFlag;
+  @GraphQLName("freeze")
+  final case object Freeze extends ResDeleteFlag;
+
+  implicit val resDeleteFlagType = deriveEnumType[ResDeleteFlag]()
+}
