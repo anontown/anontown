d7340d75b4b372dc1f5aecfacbaeab9351bc8cef
kgtkr <kgtkr.jp@gmail.com>
2020-03-20 15:50:05 +0900

    resResolvers関数に追加のフィールド渡せるようにした

--- server-scala/src/main/scala/com/anontown/application/resolvers/Res.scala
+++ server-scala/src/main/scala/com/anontown/application/resolvers/Res.scala
@@ -31,54 +31,55 @@ sealed trait Res {
 }
 
 object Res {
-  def resResolvers[A <: Res]: List[Field[Ctx, A]] = List(
-    Field(
-      "id",
-      ID.idType,
-      resolve = _.value.id
-    ),
-    Field(
-      "date",
-      dateTimeType,
-      resolve = _.value.date
-    ),
-    Field(
-      "self",
-      OptionType(BooleanType),
-      resolve = _.value.self
-    ),
-    Field(
-      "uv",
-      IntType,
-      resolve = _.value.uv
-    ),
-    Field(
-      "dv",
-      IntType,
-      resolve = _.value.dv
-    ),
-    Field(
-      "hash",
-      StringType,
-      resolve = _.value.hash
-    ),
-    Field(
-      "replyCount",
-      IntType,
-      resolve = _.value.replyCount
-    ),
-    Field(
-      "voteFlag",
-      OptionType(VoteFlag.voteFlagType),
-      resolve = _.value.voteFlag
-    )
-    // TODO: topic: Topic!
-  )
+  def resResolvers[A <: Res](fields: Field[Ctx, A]*): List[Field[Ctx, A]] =
+    List[Field[Ctx, A]](
+      Field(
+        "id",
+        ID.idType,
+        resolve = _.value.id
+      ),
+      Field(
+        "date",
+        dateTimeType,
+        resolve = _.value.date
+      ),
+      Field(
+        "self",
+        OptionType(BooleanType),
+        resolve = _.value.self
+      ),
+      Field(
+        "uv",
+        IntType,
+        resolve = _.value.uv
+      ),
+      Field(
+        "dv",
+        IntType,
+        resolve = _.value.dv
+      ),
+      Field(
+        "hash",
+        StringType,
+        resolve = _.value.hash
+      ),
+      Field(
+        "replyCount",
+        IntType,
+        resolve = _.value.replyCount
+      ),
+      Field(
+        "voteFlag",
+        OptionType(VoteFlag.voteFlagType),
+        resolve = _.value.voteFlag
+      )
+      // TODO: topic: Topic!
+    ) ++ fields.toList
 
   implicit val resType: InterfaceType[Ctx, Res] =
     InterfaceType(
       "Res",
-      () => resResolvers
+      () => resResolvers()
     )
 }
 
@@ -105,7 +106,7 @@ object ResNormal {
       name = "ResNormal",
       interfaces = interfaces(PossibleInterface[Ctx, ResNormal](Res.resType)),
       fieldsFn = () =>
-        Res.resResolvers[ResNormal] ++ List[Field[Ctx, ResNormal]](
+        Res.resResolvers[ResNormal](
           Field(
             "name",
             OptionType(StringType),
@@ -148,13 +149,10 @@ object ResHistory {
     ObjectType[Ctx, ResHistory](
       name = "ResHistory",
       interfaces = interfaces(PossibleInterface[Ctx, ResHistory](Res.resType)),
-      fieldsFn = () =>
-        Res.resResolvers[ResHistory] ++ List[Field[Ctx, ResHistory]](
-          /*
+      fieldsFn = () => Res.resResolvers[ResHistory]( /*
           TODO:
           history: History
-         */
-        )
+       */ )
     )
 }
 
@@ -175,7 +173,7 @@ object ResTopic {
     ObjectType[Ctx, ResTopic](
       name = "ResTopic",
       interfaces = interfaces(PossibleInterface[Ctx, ResTopic](Res.resType)),
-      fieldsFn = () => Res.resResolvers[ResTopic]
+      fieldsFn = () => Res.resResolvers[ResTopic]()
     )
 }
 
@@ -197,13 +195,10 @@ object ResFork {
     ObjectType[Ctx, ResFork](
       name = "ResFork",
       interfaces = interfaces(PossibleInterface[Ctx, ResFork](Res.resType)),
-      fieldsFn = () =>
-        Res.resResolvers[ResFork] ++ List[Field[Ctx, ResFork]](
-          /*
+      fieldsFn = () => Res.resResolvers[ResFork]( /*
           TODO:
           fork: TopicFork
-         */
-        )
+       */ )
     )
 }
 
@@ -226,7 +221,7 @@ object ResDelete {
       name = "ResDelete",
       interfaces = interfaces(PossibleInterface[Ctx, ResDelete](Res.resType)),
       fieldsFn = () =>
-        Res.resResolvers[ResDelete] ++ List[Field[Ctx, ResDelete]](
+        Res.resResolvers[ResDelete](
           Field(
             "flag",
             ResDeleteFlag.resDeleteFlagType,
