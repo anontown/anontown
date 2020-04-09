c170491f02990da011c3f6a8be61e02da152e6f1
kgtkr <kgtkr.jp@gmail.com>
2020-04-10 03:05:05 +0900

    build-watch-client-deps-only.shのミス修正

--- client/bin/build-watch-client-deps-only.sh
+++ client/bin/build-watch-client-deps-only.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eu
 
-npx lerna run build:watch --parallel $(npx lerna ls --scope=@anontown/client --include-filtered-dependencies | awk '{print "--scope=" $0}')
+npx lerna run build:watch --parallel $(npx lerna ls -a --scope=@anontown/client --include-filtered-dependencies | awk -F' ' '{print $1}' | grep -v ^@anontown/client$ | awk '{print "--scope=" $0}')
