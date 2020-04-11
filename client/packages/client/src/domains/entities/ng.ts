68b9f9143afd7fe4ed751ba5c9abd78f8a505ae4
kgtkr <kgtkr.jp@gmail.com>
2020-04-11 23:22:36 +0900

    importパスミス修正

--- client/packages/client/src/domains/entities/ng.ts
+++ client/packages/client/src/domains/entities/ng.ts
@@ -2,7 +2,7 @@ import { isNullish } from "@kgtkr/utils";
 import * as Im from "immutable";
 import * as uuid from "uuid";
 import * as G from "../../generated/graphql";
-import * as ngJson from "./storage-json/ng-json";
+import * as ngJson from "./storage//storage-json/ng-json";
 
 export function createDefaultNode(): NGNode {
   return {
