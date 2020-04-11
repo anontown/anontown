0feeb1a38286a926e646c7be63c69a01e5989f29
kgtkr <kgtkr.jp@gmail.com>
2020-04-11 22:07:19 +0900

    mergeAndUniqSortedArray関数を作った

---
+++ client/packages/client/src/prelude/array.ts
@@ -0,0 +1,58 @@
+import { RA, Ord, O } from "./fp-ts";
+
+/**
+ * 配列xsに配列ysをマージし、配列を返す。引数も還り値もキーで昇順にソート済みでキーの重複がない。
+ * キーが重複した要素はysの要素で置換される
+ */
+export function mergeAndUniqSortedArray<A, K>(
+  ord: Ord<K>,
+): (
+  f: (a: A) => K,
+  ys: ReadonlyArray<A>,
+) => (xs: ReadonlyArray<A>) => ReadonlyArray<A> {
+  return (f, ys) => xs => {
+    let ix = 0;
+    let iy = 0;
+    let result: Array<A> = [];
+    while (true) {
+      const ox = RA.lookup(ix, xs);
+      const oy = RA.lookup(iy, ys);
+
+      if (O.isSome(ox)) {
+        const x = ox.value;
+        if (O.isSome(oy)) {
+          const y = oy.value;
+          switch (ord.compare(f(x), f(y))) {
+            case 0: {
+              result.push(y);
+              ix++;
+              iy++;
+              break;
+            }
+            case 1: {
+              result.push(y);
+              iy++;
+              break;
+            }
+            case -1: {
+              result.push(x);
+              ix++;
+              break;
+            }
+          }
+        } else {
+          result.push(x);
+          ix++;
+        }
+      } else {
+        if (O.isSome(oy)) {
+          const y = oy.value;
+          result.push(y);
+          iy++;
+        } else {
+          return result;
+        }
+      }
+    }
+  };
+}
