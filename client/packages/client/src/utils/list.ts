adcae45431d8772eab26b6b4b05f18fdd9f5d040
kgtkr <kgtkr.jp@gmail.com>
2020-04-13 12:42:30 +0900

    updateImを消した

--- client/packages/client/src/utils/list.ts
+++ client/packages/client/src/utils/list.ts
@@ -1,5 +1,4 @@
 import { arrayImSet } from "@kgtkr/utils";
-import * as Im from "immutable";
 
 export function update<T extends { id: string }>(
   list: ReadonlyArray<T>,
@@ -8,18 +7,6 @@ export function update<T extends { id: string }>(
   const index = list.findIndex(x => x.id === item.id);
   if (index !== -1) {
     return arrayImSet(index, item)(list);
-  } else {
-    return list;
-  }
-}
-
-export function updateIm<T extends { id: string }>(
-  list: Im.List<T>,
-  item: T,
-): Im.List<T> {
-  const index = list.findIndex(x => x.id === item.id);
-  if (index !== -1) {
-    return list.set(index, item);
   } else {
     return list;
   }
