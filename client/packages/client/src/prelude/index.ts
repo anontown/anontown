256301e4a0ff0b45413f35556b0f9c7d759a3ba2
kgtkr <kgtkr.jp@gmail.com>
2020-04-13 14:43:58 +0900

    rename

--- client/packages/client/src/prelude/index.ts
+++ client/packages/client/src/prelude/index.ts
@@ -1,5 +1,5 @@
 export * from "./fp-ts";
 export * from "./collections";
 export { unwrapOption } from "../utils";
-export * as ArrayExtra from "./array";
+export * as ArrayExtra from "./array-extra";
 export * from "./raise";
