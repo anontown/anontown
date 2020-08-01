b541847134dcaa553c9613e5db9a5088a867d24d
kgtkr <kgtkr.jp@gmail.com>
2020-08-01 20:53:44 +0900

    clientのstart-watchから実行しているスクリプトに.sh付け忘れてたの直した

--- client/bin/start-watch.sh
+++ client/bin/start-watch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
 ./bin/codegen-watch.sh &
-./bin/build-watch &
-./bin/build-watch-bff &
+./bin/build-watch.sh &
+./bin/build-watch-bff.sh &
 npx lerna run start:watch --scope @anontown/bff --stream
