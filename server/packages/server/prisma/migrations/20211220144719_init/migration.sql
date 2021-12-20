3cd9447776761d5dfbbabfdd231a5e0adc92a5d0
kgtkr <contact@kgtkr.net>
2021-12-20 23:58:08 +0900

    fkeyを使わないようにした

--- server/packages/server/prisma/migrations/20211220144719_init/migration.sql
+++ server/packages/server/prisma/migrations/20211220144719_init/migration.sql
@@ -318,21 +318,3 @@ CREATE INDEX "topics_parent_id_idx" ON "topics"("parent_id");
 
 -- CreateIndex
 CREATE INDEX "topic_tags_tag_idx" ON "topic_tags"("tag");
-
--- AddForeignKey
-ALTER TABLE "tokenReqs" ADD CONSTRAINT "tokenReqs_tokenId_fkey" FOREIGN KEY ("tokenId") REFERENCES "tokens"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "reses" ADD CONSTRAINT "reses_topic_id_fkey" FOREIGN KEY ("topic_id") REFERENCES "topics"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "reses" ADD CONSTRAINT "reses_reply_id_fkey" FOREIGN KEY ("reply_id") REFERENCES "reses"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "res_votes" ADD CONSTRAINT "res_votes_res_id_fkey" FOREIGN KEY ("res_id") REFERENCES "reses"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "history_tags" ADD CONSTRAINT "history_tags_history_id_fkey" FOREIGN KEY ("history_id") REFERENCES "histories"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE "topic_tags" ADD CONSTRAINT "topic_tags_topic_id_fkey" FOREIGN KEY ("topic_id") REFERENCES "topics"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
